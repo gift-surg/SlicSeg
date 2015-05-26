dd29abb6477271122b6b270d93bebc42ddd5ab94
Guotai Wang <guotai.wang.14@ucl.ac.uk>
2015-05-26 13:12:51 +0100

    update a file

--- matlab/PossibilityConnect.m
+++ matlab/PossibilityConnect.m
@@ -5,89 +5,39 @@ function P=PossibilityConnect(I,P0,Label)
 PL=P0>=0.5;
 pSe= strel('disk',3);
 pMask=imclose(PL,pSe);
-isize=size(P0);
-L=uint8(zeros(isize));
-fg=I(find(Label>0));
+[H,W]=size(P0);
+HW=H*W;
+indexHW=uint32(zeros(HW,1));
+seedsIndex=find(Label>0);
+seeds=length(seedsIndex);
+indexHW(1:seeds)=seedsIndex(1:seeds);
+L=uint8(zeros(H,W));
+P=P0;
+L(seedsIndex)=1;
+P(seedsIndex)=1.0;
+
+fg=I(seedsIndex);
 fg_mean=mean(fg);
 fg_std=sqrt(var(double(fg)));
 fg_min=fg_mean-fg_std*3;
 fg_max=fg_mean+fg_std*2;
-seeds=0;
-indexH=uint16(zeros(isize(1)*isize(2),1));
-indexW=uint16(zeros(isize(1)*isize(2),1));
-P=P0;
-for i=1:isize(1)
-    for j=1:isize(2)
-        if(Label(i,j)>0)
-            seeds=seeds+1;
-            L(i,j)=1;
-            P(i,j)=1.0;
-            indexH(seeds,1)=i;
-            indexW(seeds,1)=j;
-        end
-    end
-end
+
 current=1;
-
 while(current<=seeds)
-    i=indexH(current,1);
-    j=indexW(current,1);
-    if(i+1<isize(1) && L(i+1,j)==0 && pMask(i+1,j)>0 && I(i+1,j)>fg_min && I(i+1,j)<fg_max)
-        L(i+1,j)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i+1;
-        indexW(seeds,1)=j;  
-
-    end
-    if(i-1>0 && L(i-1,j)==0 && pMask(i-1,j)>0 && I(i-1,j)>fg_min && I(i-1,j)<fg_max)
-        L(i-1,j)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i-1;
-        indexW(seeds,1)=j;  
-    end
-    if(j+1<isize(2) && L(i,j+1)==0 && pMask(i,j+1)>0 && I(i,j+1)>fg_min && I(i,j+1)<fg_max)
-        L(i,j+1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i;
-        indexW(seeds,1)=j+1;  
-    end
-    if(j-1>0 && L(i,j-1)==0 && pMask(i,j-1)>0 && I(i,j-1)>fg_min && I(i,j-1)<fg_max)
-        L(i,j-1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i;
-        indexW(seeds,1)=j-1;  
-    end
-    if(i+1<isize(1) && j+1<isize(2) && L(i+1,j+1)==0 && pMask(i+1,j+1)>0 && I(i+1,j+1)>fg_min && I(i+1,j+1)<fg_max)
-        L(i+1,j+1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i+1;
-        indexW(seeds,1)=j+1;  
-    end
-    if(i-1>0 && j+1<isize(2) && L(i-1,j+1)==0 && pMask(i-1,j+1)>0 && I(i-1,j+1)>fg_min && I(i-1,j+1)<fg_max)
-        L(i-1,j+1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i-1;
-        indexW(seeds,1)=j+1;  
-    end
-    if(i+1<isize(1) && j-1>0 && L(i+1,j-1)==0 && pMask(i+1,j-1)>0 && I(i+1,j-1)>fg_min && I(i+1,j-1)<fg_max)
-        L(i+1,j-1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i+1;
-        indexW(seeds,1)=j-1;  
-    end
-    if(i-1>0 && j-1>0 && L(i-1,j-1)==0 && pMask(i-1,j-1)>0 && I(i-1,j-1)>fg_min && I(i-1,j-1)<fg_max)
-        L(i-1,j-1)=1;
-        seeds=seeds+1;
-        indexH(seeds,1)=i-1;
-        indexW(seeds,1)=j-1;  
+    currentIndex=indexHW(current);
+    NeighbourIndex=[currentIndex-1,currentIndex+1,...
+        currentIndex+H,currentIndex+H-1,currentIndex+H+1,...
+        currentIndex-H,currentIndex-H-1,currentIndex-H+1];
+    for i=1:8
+        tempIndex=NeighbourIndex(i);
+        if(tempIndex>0 && tempIndex<HW && L(tempIndex)==0 && pMask(tempIndex)>0 && I(tempIndex)>fg_min && I(tempIndex)<fg_max)
+            L(tempIndex)=1;
+            seeds=seeds+1;
+            indexHW(seeds,1)=tempIndex;
+        end
     end
     current=current+1;
 end
 
-for i=1:isize(1)
-    for j=1:isize(2)
-        if(P(i,j)>0.5 && L(i,j)==0)
-            P(i,j)=0.4*P(i,j);
-        end
-    end
-end
+Lindex=find(L==0);
+P(Lindex)=P(Lindex)*0.4;
