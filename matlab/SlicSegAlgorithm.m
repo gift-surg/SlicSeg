bb01534d35d7d19ed3df791d9bbbaffec0819f82
Tom Doel <t.doel@ucl.ac.uk>
2016-05-09 20:12:54 +0100

    Folded RandomForestWrapper back into main algorithm code

--- matlab/SlicSegAlgorithm.m
+++ matlab/SlicSegAlgorithm.m
@@ -85,8 +85,8 @@ classdef SlicSegAlgorithm < CoreBaseClass
             seedLabels = obj.GetSeedLabelImage();
             currentSegIndex = obj.startIndex;
             volumeSlice = obj.volumeImage.get2DSlice(currentSegIndex, obj.orientation);
-            obj.getRandomForest.Train(seedLabels, volumeSlice);
-            [probabilitySlice, segmentationSlice] = obj.getRandomForest.PredictUsingConnectivity(seedLabels, volumeSlice, obj.lambda, obj.sigma);
+            obj.Train(seedLabels, volumeSlice);
+            [probabilitySlice, segmentationSlice] = obj.PredictUsingConnectivity(seedLabels, volumeSlice, obj.lambda, obj.sigma);
             obj.UpdateResults(currentSegIndex, segmentationSlice, probabilitySlice);
         end
         
@@ -155,9 +155,47 @@ classdef SlicSegAlgorithm < CoreBaseClass
     end
     
     methods (Access=private)
+        function Train(obj, currentTrainLabel, volumeSlice)
+            % train the random forest using scribbles in on slice
+            
+            featureMatrix = SlicSegAlgorithm.GetSliceFeature(volumeSlice);
+            if(isempty(currentTrainLabel) || isempty(find(currentTrainLabel>0)))
+                error('the training set is empty');
+            end
+            foreground=find(currentTrainLabel==127);
+            background=find(currentTrainLabel==255);
+            totalseeds=length(foreground)+length(background);
+            if(totalseeds==0)
+                error('the training set is empty');
+            end
+            TrainingSet=zeros(totalseeds,size(featureMatrix,2));
+            TrainingLabel=zeros(totalseeds,1);
+            TrainingSet(1:length(foreground),:)=featureMatrix(foreground,:);
+            TrainingLabel(1:length(foreground))=1;
+            TrainingSet(length(foreground)+1:length(foreground)+length(background),:)=featureMatrix(background,:);
+            TrainingLabel(length(foreground)+1:length(foreground)+length(background))=0;
+            TrainingDataWithLabel=[TrainingSet,TrainingLabel];
+            obj.getRandomForest.Train(TrainingDataWithLabel');
+        end
+        
+        function [probabilitySlice, segmentationSlice] = PredictUsingPrior(obj, currentSeedLabel, volumeSlice, priorSegSlice, lambda, sigma)
+            % get the probability in one slice
+            P0 = obj.Predict(volumeSlice);
+            probabilitySlice=SlicSegAlgorithm.ProbabilityProcessUsingShapePrior(P0,priorSegSlice);
+            segmentationSlice = SlicSegAlgorithm.GetSingleSliceSegmentation(currentSeedLabel, volumeSlice, probabilitySlice, lambda, sigma);
+        end
+        
+        function [probabilitySlice, segmentationSlice] = PredictUsingConnectivity(obj, currentSeedLabel, volumeSlice, lambda, sigma)
+            % get the probability in one slice
+            P0 = obj.Predict(volumeSlice);
+            probabilitySlice = SlicSegAlgorithm.ProbabilityProcessUsingConnectivity(currentSeedLabel, P0, volumeSlice);
+            segmentationSlice = SlicSegAlgorithm.GetSingleSliceSegmentation(currentSeedLabel, volumeSlice, probabilitySlice, lambda, sigma);            
+        end
+        
         function randomForest = getRandomForest(obj)
             if isempty(obj.randomForest)
-                obj.randomForest = RandomForestWrapper();
+                obj.randomForest = ForestWrapper();
+                obj.randomForest.Init(20,8,20);        
             end
             randomForest = obj.randomForest;
         end
@@ -166,11 +204,11 @@ classdef SlicSegAlgorithm < CoreBaseClass
             priorSegmentedSlice = obj.segImage.get2DSlice(priorSegIndex, obj.orientation);
             [currentSeedLabel, currentTrainLabel] = obj.UpdateSeedLabel(priorSegmentedSlice);
             if(train)
-                priorVolumeSlice = obj.volumeImage.Get2DSlice(priorSegIndex, obj.orientation);
-                obj.getRandomForest.Train(currentTrainLabel, priorVolumeSlice);
+                priorVolumeSlice = obj.volumeImage.get2DSlice(priorSegIndex, obj.orientation);
+                obj.Train(currentTrainLabel, priorVolumeSlice);
             end
-            currentVolumeSlice = obj.volumeImage.Get2DSlice(currentSegIndex, obj.orientation);
-            [probabilitySlice, segmentationSlice] = obj.getRandomForest.PredictUsingPrior(currentSeedLabel, currentVolumeSlice, priorSegmentedSlice, obj.lambda, obj.sigma);
+            currentVolumeSlice = obj.volumeImage.get2DSlice(currentSegIndex, obj.orientation);
+            [probabilitySlice, segmentationSlice] = obj.PredictUsingPrior(currentSeedLabel, currentVolumeSlice, priorSegmentedSlice, obj.lambda, obj.sigma);
             obj.UpdateResults(currentSegIndex, segmentationSlice, probabilitySlice);
         end
         
@@ -231,5 +269,102 @@ classdef SlicSegAlgorithm < CoreBaseClass
         function ResetSegmentationResultCallback(obj, ~, ~, ~)
             obj.ResetSegmentationResult();
         end
+        
+        function P0 = Predict(obj, volumeSlice)
+            featureMatrix = SlicSegAlgorithm.GetSliceFeature(volumeSlice);
+            Prob = obj.getRandomForest.Predict(featureMatrix');
+            P0 = reshape(Prob, size(volumeSlice,1), size(volumeSlice,2));
+        end        
     end
+    
+    methods (Static, Access = private) 
+        function P = ProbabilityProcessUsingShapePrior(P0,lastSeg)
+            Isize=size(lastSeg);
+            dis=zeros(Isize);
+            se= strel('disk',1);
+            temp0=lastSeg;
+            temp1=imerode(temp0,se);
+            currentdis=0;
+            while(~isempty(find(temp1>0)))
+                dis0=temp0-temp1;
+                currentdis=currentdis+1;
+                dis(dis0>0)=currentdis;
+                temp0=temp1;
+                temp1=imerode(temp0,se);
+            end
+            maxdis=currentdis;
+            
+            P=P0;
+            outsideIndex=intersect(find(dis==0),find(P>0.5));
+            P(outsideIndex)=0.4*P(outsideIndex);
+            insideIndex=intersect(find(dis>0) , find(P<0.8));
+            P(insideIndex)=P(insideIndex)+0.2*dis(insideIndex)/maxdis;
+        end
+        
+        function P = ProbabilityProcessUsingConnectivity(currentSeedLabel,P0,I)
+            PL=P0>=0.5;
+            pSe= strel('disk',3);
+            pMask=imclose(PL,pSe);
+            [H,W]=size(P0);
+            HW=H*W;
+            indexHW=uint32(zeros(HW,1));
+            seedsIndex=find(currentSeedLabel==127);
+            seeds=length(seedsIndex);
+            indexHW(1:seeds)=seedsIndex(1:seeds);
+            L=uint8(zeros(H,W));
+            P=P0;
+            L(seedsIndex)=1;
+            P(seedsIndex)=1.0;
+            
+            
+            fg=I(seedsIndex);
+            fg_mean=mean(fg);
+            fg_std=sqrt(var(double(fg)));
+            fg_min=fg_mean-fg_std*3;
+            fg_max=fg_mean+fg_std*2;
+            
+            current=1;
+            while(current<=seeds)
+                currentIndex=indexHW(current);
+                NeighbourIndex=[currentIndex-1,currentIndex+1,...
+                    currentIndex+H,currentIndex+H-1,currentIndex+H+1,...
+                    currentIndex-H,currentIndex-H-1,currentIndex-H+1];
+                for i=1:8
+                    tempIndex=NeighbourIndex(i);
+                    if(tempIndex>0 && tempIndex<HW && L(tempIndex)==0 && pMask(tempIndex)>0 && I(tempIndex)>fg_min && I(tempIndex)<fg_max)
+                        L(tempIndex)=1;
+                        seeds=seeds+1;
+                        indexHW(seeds,1)=tempIndex;
+                    end
+                end
+                current=current+1;
+            end
+            
+            Lindex=find(L==0);
+            P(Lindex)=P(Lindex)*0.4;
+        end
+        
+        function featureMatrix = GetSliceFeature(I)
+            % get the feature matrix for given slice
+            dwtFeature = image2DWTfeature(I);
+            hogFeature = image2HOGFeature(I);
+            %             lbpFeature=image2LBPFeature(I);
+            intensityFeature = image2IntensityFeature(I);
+            % glmcfeatures=image2GLCMfeature(I);
+            % featureMatrix=[intensityFeature dwtFeature];% glmcfeatures];
+            featureMatrix = [intensityFeature hogFeature dwtFeature];
+        end
+        
+        function seg = GetSingleSliceSegmentation(currentSeedLabel, currentI, currentP, lambda, sigma)
+            % use max flow to get the segmentation in one slice
+            
+            currentSeed = currentSeedLabel;
+            [flow, currentSegLabel] = wgtmaxflowmex(currentI, currentSeed, currentP, lambda, sigma);
+            currentSegLabel = 1-currentSegLabel;
+            se = strel('disk', 2);
+            currentSegLabel = imclose(currentSegLabel, se);
+            currentSegLabel = imopen(currentSegLabel, se);
+            seg = currentSegLabel(:,:);
+        end 
+    end    
 end
