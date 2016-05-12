f13bac1c3d4109e0c4089351423ffaca75b249a6
Tom Doel <t.doel@ucl.ac.uk>
2016-05-12 14:56:06 +0100

    Adjusted layout

--- matlab/library/FeatureExtract/imageGradient.cu
+++ matlab/library/FeatureExtract/imageGradient.cu
@@ -1,4 +1,4 @@
-__device__ unsigned char getPixel(const unsigned char *array,int H,int W,int i,int j)
+__device__ unsigned char getPixel(const unsigned char *array, int H, int W, int i, int j)
 {
     if(i<0 || i>=H || j<0 ||j>=W)
     {
@@ -9,7 +9,8 @@ __device__ unsigned char getPixel(const unsigned char *array,int H,int W,int i,i
         return *(array+H*j+i);
     }
 }
-__device__ void setPixel(double *array,int H,int W,int i,int j,double value)
+
+__device__ void setPixel(double *array, int H, int W, int i, int j, double value)
 {
     if(i<0 || i>=H || j<0 ||j>=W)
     {
@@ -18,15 +19,14 @@ __device__ void setPixel(double *array,int H,int W,int i,int j,double value)
     *(array+H*j+i)=value;
 }
 
-__global__ void imageGradient(const unsigned char * inputData,double *g_mag,double *g_orient,const int H,const int W)
+__global__ void imageGradient(const unsigned char * inputData, double *g_mag, double *g_orient, const int H, const int W)
 {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
-    double dx=getPixel(inputData,H,W,x+1,y)-getPixel(inputData,H,W,x-1,y);
-    double dy=getPixel(inputData,H,W,x,y-1)-getPixel(inputData,H,W,x,y+1);
+    double dx=getPixel(inputData, H, W, x+1, y)-getPixel(inputData, H, W, x-1, y);
+    double dy=getPixel(inputData, H, W, x, y-1)-getPixel(inputData, H, W, x, y+1);
     double mag=sqrt(dx*dx+dy*dy);
     double orient=atan2(dy, dx);
-    setPixel(g_mag,H,W,x,y,mag);
-    setPixel(g_orient,H,W,x,y,orient);
-    
+    setPixel(g_mag, H, W, x, y, mag);
+    setPixel(g_orient, H, W, x, y, orient);
 }
