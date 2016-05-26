2005befa03b735d23c698d01acab8523cd99da20
Tom Doel <t.doel@ucl.ac.uk>
2016-05-26 18:13:41 +0100

    Added debugging to test script

--- ci/test-win.bat
+++ ci/test-win.bat
@@ -1,4 +1,4 @@
-echo Running Windows build and test
+echo Running Windows build and testscript  %cd%/matlab/CIBuildAndTest.m
 call "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r "try, run('%cd%/matlab/CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
 
 if not "%ERRORLEVEL%" == "0" (
