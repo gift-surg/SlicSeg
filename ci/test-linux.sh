def9d74a9dc63eb3ebfbab7db1072bb1609af303
Tom Doel <t.doel@ucl.ac.uk>
2016-05-26 18:06:32 +0100

    Removed unused line

--- ci/test-linux.sh
+++ ci/test-linux.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/local/R2015b/bin/matlab -nodisplay -nosplash -nodesktop -r "try, run('$(pwd)/matlab/CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
-#mvn install -B
 if [ $? -eq 0 ]; then
 	echo "Success running CIBuildAndTest.m"
 	exit 0;
