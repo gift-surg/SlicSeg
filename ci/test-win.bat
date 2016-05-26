caa8f22ac1b5b29c988328b0d7dd2d2f62d76a3a
Tom Doel <t.doel@ucl.ac.uk>
2016-05-26 18:14:57 +0100

    Fix paths in CI script

--- ci/test-win.bat
+++ ci/test-win.bat
@@ -1,5 +1,5 @@
 echo Running Windows build and testscript  %cd%/matlab/CIBuildAndTest.m
-call "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r "try, run('%cd%/matlab/CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
+call "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r "try, run('%cd%\matlab\CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
 
 if not "%ERRORLEVEL%" == "0" (
     echo Exit Code = %ERRORLEVEL%
