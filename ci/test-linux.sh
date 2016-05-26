98416182996d501df6c25fe53199ed1363f219f4
Tom Doel <t.doel@ucl.ac.uk>
2016-05-26 15:37:42 +0100

    Submitted a test runner script for linux

---
+++ ci/test-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/user/local/R2015b.app/bin/matlab -nodisplay -nosplash -nodesktop -r "try, run('$(pwd)/matlab/CIBuildAndTest.m'), catch, exit(1), end, exit(0); "
+#mvn install -B
+if [ $? -eq 0 ]; then
+	echo "Success running CIBuildAndTest.m"
+	exit 0;
+else
+	echo "Failure running CIBuildAndTest.m"
+	echo 1;
+fi
