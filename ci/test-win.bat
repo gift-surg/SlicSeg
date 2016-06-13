a8f186fad1550afd996b0d5e82b4cd8a3efe824e
Tom Doel <t.doel@ucl.ac.uk>
2016-06-13 14:26:24 +0100

    Fix windows CI batch script

--- ci/test-win.bat
+++ ci/test-win.bat
@@ -1,5 +1,5 @@
-echo Running Windows build and testscript  %~dp0%\matlab\CIBuildAndTest.m
-call "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r "try, run('%~dp0%\matlab\CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
+echo Running Windows build and testscript  %~dp0..\\matlab\CIBuildAndTest.m
+call "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r "try, run('%~dp0..\matlab\CIBuildAndTest.m'), catch ex, disp(['Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
 
 if not "%ERRORLEVEL%" == "0" (
     echo Exit Code = %ERRORLEVEL%
