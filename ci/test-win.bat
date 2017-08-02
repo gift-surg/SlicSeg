ae1172eb398acefcbe3738aec459fb1fc46b06f9
Tom Doel <t.doel@ucl.ac.uk>
2017-08-02 13:05:58 +0100

    Updated windows test script to output to log file and display

--- ci/test-win.bat
+++ ci/test-win.bat
@@ -1,9 +1,12 @@
+setlocal EnableDelayedExpansion
 echo Running Windows build and testscript  %~dp0..\matlab\CIBuildAndTest.m
 echo Path: %PATH%
 set MATCOMMAND="try, run('%~dp0..\matlab\CIBuildAndTest.m'), catch ex, system(['ECHO Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
-"C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -r %MATCOMMAND%
+"C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -logfile ci-output.log -r %MATCOMMAND%
+set LEVEL=!ERRORLEVEL!
+type ci-output.log
 
-if not "%ERRORLEVEL%" == "0" (
-    echo Exit Code = %ERRORLEVEL%
+if not "!LEVEL!" == "0" (
+    echo ERROR: Exit Code = !LEVEL!
 	exit /b 1
 )
