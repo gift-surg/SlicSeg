c491be733fc1f2c169574665d227e9585c8876a7
Tom Doel <t.doel@ucl.ac.uk>
2018-01-02 16:20:47 +0000

    Add file check before printing log file

--- ci/test-win.bat
+++ ci/test-win.bat
@@ -4,7 +4,12 @@ echo Path: %PATH%
 set MATCOMMAND="try, run('%~dp0..\matlab\CIBuildAndTest.m'), catch ex, system(['ECHO Exception during CIBuildAndTest.m: ' ex.message]), exit(1), end, exit(0);"
 "C:\Program Files\MATLAB\R2015b\bin\matlab.exe" -wait -nodisplay -nosplash -nodesktop -logfile ci-output.log -r %MATCOMMAND%
 set LEVEL=!ERRORLEVEL!
-type ci-output.log
+if exist ci-output.log (
+    type ci-output.log
+) else (
+    echo Log file not found
+)
+
 
 if not "!LEVEL!" == "0" (
     echo ERROR: Exit Code = !LEVEL!
